7f1844c199adfdebc301148e67b2fc90a90715c9
Oliver <5808418+phoebos02@users.noreply.github.com>
2025-07-09 23:38:02 +0200

    try to fix test allowing sockets

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
-# import pytest
+import pytest
+from pytest_socket import enable_socket, socket_allow_hosts
 
 pytest_plugins = "pytest_homeassistant_custom_component"
 
@@ -12,3 +13,9 @@ sys.path.insert(0, project_root)
 # def enable_custom_integrations(enable_custom_integrations):
 #    """Enable loading of custom integrations."""
 #    yield
+
+@pytest.hookimpl(trylast=True)
+def pytest_runtest_setup():
+    """Re-enable sockets and allow specific hosts before each test."""
+    enable_socket()
+    socket_allow_hosts(["127.0.0.1", "152.88.10.20"], allow_unix_socket=True)
